1f3a386811da270d5fb799d43b17a392969d45c4
kamax1226 <karlikboroda@gmail.com>
2022-04-03 20:00:34 +0300

    Add the style to component

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,29 @@
 import * as React from "react"
 import { Link } from "gatsby"
+import { 
+    container,
+    heading,
+    navLinks,
+    navLinkText,
+    navLinkItem
+} from "./layout.module.css"
 
 const Layout = ({ pageTitle, children }) => {
     return(
-        <div>
+        <div className={container}>
             <title>{pageTitle}</title>
-            <nav>
+            <nav className={navLinks}>
                 <ul>
-                    <li><Link to="/">Dashboard</Link></li>
-                    <li><Link to="/about">About Me</Link></li>
+                    <li className={navLinkItem}>
+                        <Link to="/" className={navLinkText}>Dashboard</Link>
+                    </li>
+                    <li className={navLinkItem}>
+                        <Link to="/about" className={navLinkText}>About Me</Link>
+                    </li>
                 </ul>
             </nav>
             <main>
-                <h1>{pageTitle}</h1>
+                <h1 className={heading}>{pageTitle}</h1>
                 {children}
             </main>
         </div>
