97b74e575ca2e26962d7e9ccb519e5b25c20f5e3
kamax1226 <karlikboroda@gmail.com>
2022-04-03 19:18:50 +0300

    Add the About page

---
+++ src/pages/about.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+import { Link } from "gatsby"
+
+const AboutPage = () => {
+    return (
+        <main>
+            <title>About Me</title>
+            <h1>About Me</h1>
+            <Link to="/">Dashboard</Link>
+            <p>Hi There! It is the enjoying time</p>
+        </main>
+    )
+}
+
+export default AboutPage;
